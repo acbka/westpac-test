5b02f95c9f73ad09007e31aee8d498d502e4bd42
Vlada <vladochka.vb@gmail.com>
2023-08-10 09:13:44 +1200

    update validateNumber

--- src/utils/validateNumber/validateNumber.ts
+++ src/utils/validateNumber/validateNumber.ts
@@ -3,8 +3,5 @@ import { getCharCodes } from "../getCharCodes/getCharCodes";
 export const validateNumber = (password: string): boolean => {
   const passwordCharCodes = getCharCodes(password);
 
-  return (
-    passwordCharCodes.find((charCode) => charCode >= 48 && charCode <= 57) !==
-    undefined
-  );
+  return passwordCharCodes.some((charCode) => charCode >= 48 && charCode <= 57);
 };
